762d479e1648d14985175c9054b7e0953882ca28
huangliang796-source <huangliang796@gmail.com>
2026-09-15 16:47:36 +0800

    Create convert_IBS_distance_matrix_into_phylip_input_format.pl

---
+++ 01_population_structure/Admixture_and_NJ_tree/convert_IBS_distance_matrix_into_phylip_input_format.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+####this script is usd to convert the PLINk generated IBS metrix to phylip format
+#####$ARGv[0]=plink.mdist $ARGV[1]=the ped file
+die usage() unless @ARGV==2;
+my ($ibs_matrix,$ped) = @ARGV;
+
+my $num = 0;
+my  @inbred_name;
+open ALIAS, $ped or die "$!";
+
+while (<ALIAS>){
+    my ($fam,$tem_alias) = split(/\t/,$_);
+    ++$num;
+}
+
+print $num,"\n";
+my $i = 1000;
+open T, "$ibs_matrix" or die "$!";
+while (<T>){
+    my @matrix = split(/\s+/,$_);
+    for(my $j =0;$j<@matrix;++$j){
+        $matrix[$j] = sprintf "%0.6f",$matrix[$j];
+    }
+    my $matrix = join(" ",@matrix);
+    print "maize$i        $matrix\n";
+    $i++;
+}
+
+
+sub usage{
+    print <<DIE;
+    perl .pl <plink IBS metrix> <Inbred line pedgree file>
+
+DIE
+   exit 1;
